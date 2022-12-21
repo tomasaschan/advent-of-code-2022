fd5837c011f2285e4345c67ac53bc111e818df1d
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2022-12-21 12:05:24 +0100

    Tweak helpers

--- new.sh
+++ new.sh
@@ -5,3 +5,4 @@ day=${1?"usage: $0 <day>"}
 mkdir -p src/dec$day
 echo "#!/usr/bin/env python3" > src/dec$day/solve.py
 chmod +x src/dec$day/solve.py
+touch src/dec$day/input.txt
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 day=${1?"usage: $0 <day>"}
+input=${2:-'input'}
 
-find src/dec$day -type f | entr -rc sh -c "./src/dec$day/solve.py < src/dec$day/input.txt"
+find src/dec$day -type f | entr -rc sh -c "./src/dec$day/solve.py < src/dec$day/$input.txt"
