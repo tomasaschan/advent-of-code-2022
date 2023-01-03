99c122e9283b82cbc6ef6c01a34241a9c6560024
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2023-01-03 22:19:41 +0100

    Add debug output support

--- run.sh
+++ run.sh
@@ -2,5 +2,11 @@
 
 day=${1?"usage: $0 <day>"}
 input=${2:-'input'}
+debug=
+if [[ ${3:-''} == "debug" ]]; then
+    debug="2> src/dec$day/debug.out"
+elif [[ ${3:-''} == "nodebug" ]]; then
+    debug="2> /dev/null"
+fi
 
-find src/dec$day -type f | entr -rc sh -c "./src/dec$day/solve.py < src/dec$day/$input.txt"
+find src/dec$day -type f -name '*.py' -or -name '*.txt' | entr -rc sh -c "./src/dec$day/solve.py < src/dec$day/$input.txt $debug"
